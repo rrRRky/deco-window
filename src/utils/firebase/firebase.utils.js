2b0437549d63e0e970609a43efb988d985274307
Rakesh Yadav <yadav94rakesh@gmail.com>
2023-05-06 16:23:36 +0530

    sign in and sign up

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
         signInWithRedirect, 
         signInWithPopup, 
         GoogleAuthProvider,
+        createUserWithEmailAndPassword
     } from 'firebase/auth';
 import {
     getFirestore,
@@ -25,18 +26,23 @@ const firebaseConfig = {
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
+const googleprovider = new GoogleAuthProvider();
 
-provider.setCustomParameters({
+googleprovider.setCustomParameters({
     prompt : "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleprovider);
+export const signInWithGoogleRedirect = () => signInWithRedirect (auth, googleprovider);
+
 
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+    
+    if (!userAuth) return;
+    
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
 
@@ -56,3 +62,11 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     }
     return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+
+    if (!email || !password) return;
+
+   return await createUserWithEmailAndPassword (auth, email, password);
+
+}
